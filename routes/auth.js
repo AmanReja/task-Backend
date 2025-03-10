83ab2e3554d5a311eefd0d9ddea1459293eeb002
Aman Reja <amnmolla742@gmail.com>
2025-03-10 12:40:53 +0530

    fix

--- routes/auth.js
+++ routes/auth.js
@@ -1,18 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const {
-  login,
-  signup,
-  checkauth,
-  logout
-} = require("../controllers/auth_controller");
+const { login, signup } = require("../controllers/auth_controller");
 const { assign } = require("../controllers/assignment_controller");
-const { secureRoutes } = require("../middleware/secureRoutes");
 
 router.post("/signup", signup);
 router.post("/login", login);
-router.post("/logout", logout);
 router.post("/assign", assign);
-router.get("/checkauth", secureRoutes, checkauth);
 
 module.exports = router;
