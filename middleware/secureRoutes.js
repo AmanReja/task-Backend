0ee9abbbb080f160098a8b9c8e83325de20f55ae
Aman Reja <amnmolla742@gmail.com>
2025-03-11 11:24:17 +0530

    fix

--- middleware/secureRoutes.js
+++ middleware/secureRoutes.js
@@ -3,38 +3,30 @@ const Admin = require("../models/adminSchema");
 const dotenv = require("dotenv").config();
 
 const secureRoutes = async (req, res, next) => {
-  let token = req.cookies.jwt; // First try getting from cookies
-
-  // If the token is not found in cookies, check the Authorization header
-  if (!token && req.headers.authorization) {
-    const authHeader = req.headers.authorization;
-    if (authHeader.startsWith("Bearer ")) {
-      token = authHeader.split(" ")[1]; // Extract token from "Bearer <token>"
-    }
-  }
+  const token = req.cookies.jwt;
 
   if (!token) {
-    return res.status(401).json({ message: "No token provided" });
+    return res.status(401).json({ message: "Don't have any token" });
   }
 
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const decoded = jwt.verify(token, process.env.JWT_SECRAT);
 
     if (!decoded) {
-      return res.status(403).json({ message: "Unauthorized: Invalid token" });
+      return res.status(403).json({ message: "unauthorized Invalid token" });
     }
 
     const admin = await Admin.findById(decoded.adminid).select("-adminpass");
 
     if (!admin) {
-      return res.status(404).json({ message: "User not found" });
+      return res.status(405).json({ message: " user not found" });
     }
-
     req.admin = admin;
     next();
   } catch (error) {
-    console.log("Error in secureRoutes middleware:", error.message);
-    return res.status(500).json({ message: "Internal Server Error" });
+    console.log("error in sucure rout middleware", error.message);
+
+    return res.status(500).json({ message: "internalServer error" });
   }
 };
 
